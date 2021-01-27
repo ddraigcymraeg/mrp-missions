98ea598d0a82b063300890696bc4162a0792d3ef
ddraigcymraeg <gkrantz@gmail.com>
2021-01-27 17:15:04 -0500

    scoreboard sorted by total mission money or money per mission
    scoreboard sorted by total mission money or money per mission

--- mrp-missions-v3/scoreboard.lua
+++ mrp-missions-v3/scoreboard.lua
@@ -1,7 +1,29 @@
 local listOn = false
+
+--1 = By Total Money, 2 = Money per Mission
+local sortType=2
+
 function round(num, numDecimalPlaces)
   local mult = 10^(numDecimalPlaces or 0)
   return math.floor(num * mult + 0.5) / mult
+end
+
+
+function getKeysSortedByValue(tbl, sortFunction)
+  local keys = {}
+  for key in pairs(tbl) do
+    table.insert(keys, key)
+  end
+
+  table.sort(keys, function(a, b)
+	if sortType==2 then
+		return sortFunction(tonumber(tbl[a]["MoneyPerMission"]), tonumber(tbl[b]["MoneyPerMission"]))
+	elseif sortType==1 then
+		return sortFunction(tonumber(tbl[a]["TotalMoney"]), tonumber(tbl[b]["TotalMoney"]))
+	end
+  end)
+
+  return keys
 end
 
 Citizen.CreateThread(function()
@@ -15,17 +37,38 @@ Citizen.CreateThread(function()
 				if not IsPlayerFreeAiming(PlayerId()) then
 				
 					local players = {}
+					local allstats = {}
 					ptable = GetPlayers()
 					for _, i in ipairs(ptable) do
+						
 						--local wantedLevel = GetPlayerWantedLevel(i)
 						local totalMoney = DecorGetInt(GetPlayerPed(i),"mrpplayermoney")
 						local totalMissions = DecorGetInt(GetPlayerPed(i),"mrpplayermissioncount")
 						if totalMissions == 0 then totalMissions = 1 end
 						local moneypermission = round((totalMoney/totalMissions),2) 
-						r, g, b = GetPlayerRgbColour(i)
-						table.insert(players, 
-						'<tr style=\"color: rgb(' .. r .. ', ' .. g .. ', ' .. b .. ')\"><td>' .. sanitize(GetPlayerName(i)) .. '</td><td>' .. tostring(totalMissions) .. '</td><td>$' .. (tostring(totalMoney)) .. '</td><td>$' .. (string.format("%.2f",tostring(moneypermission))) .. '</td></tr>'
+						--if 
+						--r, g, b = GetPlayerRgbColour(i)
+						--table.insert(players, 
+					--	'<tr style=\"color: rgb(' .. r .. ', ' .. g .. ', ' .. b .. ')\"><td>' .. sanitize(GetPlayerName(i)) .. '</td><td>' .. tostring(totalMissions) .. '</td><td>$' .. (tostring(totalMoney)) .. '</td><td>$' .. (string.format("%.2f",tostring(moneypermission))) .. '</td></tr>'
+					--	)
+						-- player name, total money, totalmissions, moneypermission,isplayer 
+						local isplayer = 0 
+						if GetPlayerPed(i) == GetPlayerPed(-1) then
+							isplayer=1
+						end
+						local stats = {Name=sanitize(GetPlayerName(i)),TotalMoney=tostring(totalMoney),TotalMissions=tostring(totalMissions),MoneyPerMission=(string.format("%.2f",tostring(moneypermission))),isplayer=isplayer,playerindex=i}
+						table.insert(allstats,stats)
+					end
+					
+					
+					local sortedKeys = getKeysSortedByValue(allstats, function(a, b) return a > b end)
+					for _, key in ipairs(sortedKeys) do
+						print(key, allstats[key]["MoneyPerMission"])
+					r, g, b = GetPlayerRgbColour(allstats[key]["playerindex"])
+					table.insert(players, 
+						'<tr style=\"color: rgb(' .. r .. ', ' .. g .. ', ' .. b .. ')\"><td>' .. allstats[key]["Name"] .. '</td><td>' .. allstats[key]["TotalMissions"] .. '</td><td>$' .. allstats[key]["TotalMoney"] .. '</td><td>$' .. allstats[key]["MoneyPerMission"] .. '</td></tr>'
 						)
+					
 					end
 					
 					SendNUIMessage({ text = table.concat(players) })
