941ec72f01d3bef3fb48d9002ad269ace521d5ce
ddraigcymraeg <gkrantz@gmail.com>
2024-11-04 15:01:54 -0500

    Update __resource.lua
    added
    
    resource_type 'map' { gameTypes = { fivem = true } }
    
    map 'map.lua'

--- mrp-missions-v3/__resource.lua
+++ mrp-missions-v3/__resource.lua
@@ -1,10 +1,12 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 --resource_manifest_version 'f15e72ec-3972-4fe4-9c7d-afc5394ae207'
 --resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-
-
-
 --resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9 (2017-06-04)'
+
+resource_type 'map' { gameTypes = { fivem = true } }
+
+map 'map.lua'
+
 client_script 'client.lua'
 client_script 'missions.lua'
 
