4bca779bb80269cf18fcf25d2e0c45fce414334a
ddraigcymraeg <gkrantz@gmail.com>
2024-11-06 15:34:20 -0500

    Update __resource.lua
    added telportcommands.lua

--- mrp-missions-v3/__resource.lua
+++ mrp-missions-v3/__resource.lua
@@ -10,6 +10,7 @@ map 'map.lua'
 client_script 'basic_client.lua'
 client_script 'client.lua'
 client_script 'missions.lua'
+client_script 'teleportcommands.lua'
 
 -- temporary!
 ui_page 'html/scoreboard.html'
